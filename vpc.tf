c9c33792f19e582bcf6352f815afa4f32e8f3c58
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2025-11-15 15:43:58 +0100

    adding subnet

--- vpc.tf
+++ vpc.tf
@@ -6,3 +6,13 @@ resource "aws_vpc" "wordpress_vpc" {
     Name = "wordpress_vpc"
   }
 }
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                    = aws_vpc.wordpress_vpc.id
+  cidr_block                = "10.0.1.0/24"
+  availability_zone         = "eu-central-1a"
+  map_public_ip_on_launch   = true
+  tags = {
+    Name = "public_subnet_wordpress_1"
+  }
+}
