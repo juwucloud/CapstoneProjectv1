9cd7a456488e7e5c25a803f334aa316049882752
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2025-11-15 15:29:42 +0100

    adding vpc for wordpress server

---
+++ vpc.tf
@@ -0,0 +1,8 @@
+ressource "aws_vpc" "wordpress_vpc" {
+  cidr_block            = "10.0.0.0/16"
+  enable_dns_support    = true
+  enable_dns_hostnames  = true
+  tags = {
+    Name = "wordpress_vpc"
+  }
+}
