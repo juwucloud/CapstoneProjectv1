7c7d337dc1bd20f1f4a8b0d74b6fc94d492e4a4b
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2025-11-17 15:37:10 +0100

    changed wordpress to capstone in all files

--- internetgateway.tf
+++ internetgateway.tf
@@ -1,5 +1,5 @@
 resource "aws_internet_gateway" "WPig" {
-  vpc_id = aws_vpc.wordpress_vpc.id
+  vpc_id = aws_vpc.capstone_vpc.id
 
   tags = {
     Name = "WPig"
--- vpc-subnet.tf
+++ vpc-subnet.tf
@@ -1,49 +1,49 @@
 # Create VPC 
 
-resource "aws_vpc" "wordpress_vpc" {
+resource "aws_vpc" "capstone_vpc" {
   cidr_block            = "10.0.0.0/16"
   enable_dns_support    = true
   enable_dns_hostnames  = true
   tags = {
-    Name = "wordpress_vpc"
+    Name = "capstone_vpc"
   }
 }
 
 # Create 2 Public and 2 Private Subnets for high availability
 
 resource "aws_subnet" "public_subnet_1" {
-  vpc_id                    = aws_vpc.wordpress_vpc.id
+  vpc_id                    = aws_vpc.capstone_vpc.id
   cidr_block                = "10.0.0.0/24"
   availability_zone         = var.aws_availability_zone_a
   map_public_ip_on_launch   = true
   tags = {
-    Name = "public_subnet_wordpress_1"
+    Name = "public_subnet_capstone_1"
   }
 }
 resource "aws_subnet" "private_subnet_1" {
-  vpc_id                    = aws_vpc.wordpress_vpc.id
+  vpc_id                    = aws_vpc.capstone_vpc.id
   cidr_block                = "10.0.1.0/24"
   availability_zone         = var.aws_availability_zone_a
   map_public_ip_on_launch   = false
   tags = {
-    Name = "private_subnet_wordpress_1"
+    Name = "private_subnet_capstone_1"
   }
 }
 resource "aws_subnet" "public_subnet_2" {
-  vpc_id                    = aws_vpc.wordpress_vpc.id
+  vpc_id                    = aws_vpc.capstone_vpc.id
   cidr_block                = "10.0.2.0/24"
   availability_zone         = var.aws_availability_zone_b
   map_public_ip_on_launch   = true
   tags = {
-    Name = "public_subnet_wordpress_2"
+    Name = "public_subnet_capstone_2"
   }
 }
 resource "aws_subnet" "private_subnet_2" {
-  vpc_id                    = aws_vpc.wordpress_vpc.id
+  vpc_id                    = aws_vpc.capstone_vpc.id
   cidr_block                = "10.0.3.0/24"
   availability_zone         = var.aws_availability_zone_b
   map_public_ip_on_launch   = false
   tags = {
-    Name = "private_subnet_wordpress_2"
+    Name = "private_subnet_capstone_2"
   }
 }
