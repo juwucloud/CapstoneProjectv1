8a04516b6dbd416d91e3b5d1591352b0021f9e6a
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2025-11-17 15:55:07 +0100

    added security groups

--- securitygroups.tf
+++ securitygroups.tf
@@ -1,11 +1,11 @@
 # SSH - Security Group
 resource "aws_security_group" "ssh_sg" {
-  name   = "SSH Security Group"
-  vpc_id = aws_vpc.capstone_vpc.id
+  name        = "SSH Security Group"
+  vpc_id      = aws_vpc.capstone_vpc.id
   description = "Allow SSH access to EC2 instances"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ssh_sg_rule" {
+resource "aws_vpc_security_group_ingress_rule" "ssh_sg_inbound_rule" {
   security_group_id = aws_security_group.ssh_sg.id
 
   cidr_ipv4   = "0.0.0.0/0"
@@ -15,8 +15,33 @@ resource "aws_vpc_security_group_ingress_rule" "ssh_sg_rule" {
   description = "Allow SSH access to EC2 instances"
 }
 
-resource "aws_vpc_security_group_egress_rule" "ssh_outbound_all_traffic" {
+resource "aws_vpc_security_group_egress_rule" "ssh_sg_outbound_rule" {
   security_group_id = aws_security_group.ssh_sg.id
+
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # means "to all ports"
+}
+
+# HTTP - Security Group
+resource "aws_security_group" "http_sg" {
+  name        = "HTTP Security Group"
+  vpc_id      = aws_vpc.capstone_vpc.id
+  description = "Allow HTTP access to EC2 instances"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http_sg_inbound_rule" {
+  security_group_id = aws_security_group.http_sg.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+  description = "Allow HTTP access to EC2 instances"
+}
+
+resource "aws_vpc_security_group_egress_rule" "http_sg_outbound_rule" {
+  security_group_id = aws_security_group.http_sg.id
+
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # means "to all ports"
 }
