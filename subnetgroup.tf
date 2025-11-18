babf1692838bfd45b854c684460ccc9e588c1e11
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2025-11-18 16:02:49 +0100

    fix subnetgroup issue

--- subnetgroup.tf
+++ subnetgroup.tf
@@ -1,7 +1,6 @@
 resource "aws_db_subnet_group" "rds_subnets" {
-  name       = "rds_subnets"
-  subnet_ids = var.subnet_ids
-
+  name       = "rds_subnet_group"
+  subnet_ids = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
   tags = {
     Name = "rds_subnets"
   }
