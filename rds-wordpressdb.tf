bb4692a82b533f5fb8eb3adf2cf6e242fe1f8dc2
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2025-11-19 11:07:45 +0100

    added identifier to rds

--- rds-wordpressdb.tf
+++ rds-wordpressdb.tf
@@ -2,6 +2,7 @@ resource "aws_db_instance" "wordpressdb" {
   allocated_storage    = 20
   engine               = "mysql"
   engine_version       = null # gets the latest version
+  identifier           = "jw-capstone-wordpressdb"
   instance_class       = "db.t3.micro"
   username             = var.db_user
   password             = var.dbuser_password
